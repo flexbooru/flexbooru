260607b601240364b055150feb1a002015bd7c92
onlymash <im@fiepi.me>
2019-02-07 01:43:36 +0800

    Fix toolbar title

--- app/src/main/java/onlymash/flexbooru/ui/BrowseActivity.kt
+++ app/src/main/java/onlymash/flexbooru/ui/BrowseActivity.kt
@@ -3,6 +3,7 @@ package onlymash.flexbooru.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_browse.*
 import kotlinx.android.synthetic.main.toolbar.*
 import onlymash.flexbooru.Constants
@@ -20,8 +21,11 @@ class BrowseActivity : AppCompatActivity() {
         private const val TAG = "BrowseActivity"
     }
     private var startId = -1
+    private var postsDan: MutableList<PostDan>? = null
+    private var postsMoe: MutableList<PostMoe>? = null
     private val postLoadedListener: PostLoadedListener = object : PostLoadedListener {
         override fun onDanItemsLoaded(posts: MutableList<PostDan>) {
+            postsDan = posts
             var position = 0
             if (startId >= 0) {
                 posts.forEachIndexed { index, postDan ->
@@ -32,10 +36,12 @@ class BrowseActivity : AppCompatActivity() {
                 }
             }
             pagerAdapter.updateData(posts, Constants.TYPE_DANBOORU)
+            toolbar.title = String.format(getString(R.string.browse_toolbar_title_and_id), posts[position].id)
             pager_browse.currentItem = position
         }
 
         override fun onMoeItemsLoaded(posts: MutableList<PostMoe>) {
+            postsMoe = posts
             var position = 0
             if (startId >= 0) {
                 posts.forEachIndexed { index, postMoe ->
@@ -46,11 +52,32 @@ class BrowseActivity : AppCompatActivity() {
                 }
             }
             pagerAdapter.updateData(posts, Constants.TYPE_MOEBOORU)
+            toolbar.title = String.format(getString(R.string.browse_toolbar_title_and_id), posts[position].id)
             pager_browse.currentItem = position
         }
     }
 
     private lateinit var pagerAdapter: BrowsePagerAdapter
+
+    private val pagerChangeListener = object : ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) {
+
+        }
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            val id = when {
+                postsDan != null -> postsDan!![position].id
+                postsMoe != null -> postsMoe!![position].id
+                else -> -1
+            }
+            if (id > 0) toolbar.title = String.format(getString(R.string.browse_toolbar_title_and_id), id)
+        }
+
+        override fun onPageSelected(position: Int) {
+
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +91,7 @@ class BrowseActivity : AppCompatActivity() {
         Log.e(TAG, "id: $startId")
         pagerAdapter = BrowsePagerAdapter(GlideApp.with(this))
         pager_browse.adapter = pagerAdapter
+        pager_browse.addOnPageChangeListener(pagerChangeListener)
         val loader = ServiceLocator.instance().getPostLoader()
         loader.setPostLoadedListener(postLoadedListener)
         when (type) {
