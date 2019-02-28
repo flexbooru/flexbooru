1a569f7a93081f4924fa581bbef10101d1955fc3
onlymash <im@fiepi.me>
2019-02-28 19:11:52 +0800

    Add AppUpdaterApi

---
+++ app/src/main/java/onlymash/flexbooru/api/AppUpdaterApi.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2019 by onlymash <im@fiepi.me>, All rights reserved
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package onlymash.flexbooru.api
+
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import onlymash.flexbooru.Constants
+import onlymash.flexbooru.util.UserAgent
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import java.util.concurrent.TimeUnit
+
+interface AppUpdaterApi {
+
+    companion object {
+        private const val BASE_URL = "https://raw.githubusercontent.com"
+        fun create(): AppUpdaterApi {
+
+            val logger = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { log ->
+                Log.d("AppUpdaterApi", log)
+            }).apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }
+
+            val interceptor = Interceptor { chain ->
+                val requests =  chain.request().newBuilder()
+                    .removeHeader(Constants.USER_AGENT_KEY)
+                    .addHeader(Constants.USER_AGENT_KEY, UserAgent.get())
+                    .build()
+                chain.proceed(requests)
+            }
+
+            val client = OkHttpClient.Builder().apply {
+                connectTimeout(10, TimeUnit.SECONDS)
+                readTimeout(10, TimeUnit.SECONDS)
+                writeTimeout(15, TimeUnit.SECONDS)
+                    .addInterceptor(interceptor)
+                    .addInterceptor(logger)
+            }
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(AppUpdaterApi::class.java)
+        }
+    }
+
+    @GET("/flexbooru/flexbooru/master/app/update.json")
+    fun checkUpdate(): Call<UpdateInfo>
+}
+
+data class UpdateInfo(
+    val version_code: Long,
+    val version_name: String,
+    val url: String
+)
