6c69fb65a5411832014acc8ac272f1b75080e8eb
onlymash <im@fiepi.me>
2019-03-15 12:55:28 +0800

    Update market link

--- app/src/main/java/onlymash/flexbooru/ui/fragment/AboutFragment.kt
+++ app/src/main/java/onlymash/flexbooru/ui/fragment/AboutFragment.kt
@@ -84,7 +84,7 @@ class AboutFragment : PreferenceFragment() {
             "about_app_rate" -> {
                 try {
                     requireContext().startActivity(Intent.createChooser(
-                        Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${requireContext().applicationContext.packageName}")),
+                        Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=onlymash.flexbooru.play")),
                         getString(R.string.share_via)))
                 } catch (_: ActivityNotFoundException) { }
             }
