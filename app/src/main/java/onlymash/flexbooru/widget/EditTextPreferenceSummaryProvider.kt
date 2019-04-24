da172c3c9d2c1a9a72c97a264263310534b7ec7d
onlymash <im@fiepi.me>
2019-04-24 15:23:44 +0800

    Use default SummaryProvider

--- app/src/main/java/onlymash/flexbooru/widget/EditTextPreferenceSummaryProvider.kt
+++
@@ -1,9 +0,0 @@
-package onlymash.flexbooru.widget
-
-import androidx.preference.EditTextPreference
-import androidx.preference.Preference
-
-class EditTextPreferenceSummaryProvider : Preference.SummaryProvider<EditTextPreference> {
-    override fun provideSummary(preference: EditTextPreference?): CharSequence =
-            preference?.text ?: ""
-}
