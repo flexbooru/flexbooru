ca289e9214c4f7b09586347db6efd4b93db70248
onlymash <im@fiepi.me>
2019-05-28 18:04:17 +0800

    Update window by ext

--- app/src/main/java/onlymash/flexbooru/ui/BrowseActivity.kt
+++ app/src/main/java/onlymash/flexbooru/ui/BrowseActivity.kt
@@ -17,7 +17,6 @@ package onlymash.flexbooru.ui
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.net.Uri
@@ -26,7 +25,6 @@ import android.os.Handler
 import android.provider.DocumentsContract
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.widget.TooltipCompat
 import androidx.core.app.ActivityOptionsCompat
@@ -293,7 +291,7 @@ class BrowseActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initWindow()
+        window.initFullTransparentBar()
         setContentView(R.layout.activity_browse)
         pageType = intent?.getIntExtra(Constants.PAGE_TYPE_KEY, Constants.PAGE_TYPE_POST) ?: Constants.PAGE_TYPE_POST
         colorDrawable = ColorDrawable(ContextCompat.getColor(this, R.color.black))
@@ -674,13 +672,13 @@ class BrowseActivity : BaseActivity() {
     private fun setBg() {
         when (toolbar.visibility) {
             View.VISIBLE -> {
-                hideBar()
+                window.hideBar()
                 toolbar.visibility = View.GONE
                 bottom_bar_container.visibility = View.GONE
                 shadow.visibility = View.GONE
             }
             else -> {
-                showBar()
+                window.showBar()
                 toolbar.visibility = View.VISIBLE
                 bottom_bar_container.visibility = View.VISIBLE
                 shadow.visibility = View.VISIBLE
@@ -690,35 +688,11 @@ class BrowseActivity : BaseActivity() {
 
     private fun resetBg() {
         if (toolbar.visibility == View.GONE) {
-            showBar()
+            window.showBar()
             toolbar.visibility = View.VISIBLE
             bottom_bar_container.visibility = View.VISIBLE
             shadow.visibility = View.VISIBLE
         }
-    }
-
-    private fun initWindow() {
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-        window.statusBarColor = Color.TRANSPARENT
-        window.navigationBarColor = Color.TRANSPARENT
-        showBar()
-    }
-
-    private fun showBar() {
-        val uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.decorView.systemUiVisibility = uiFlags
-    }
-
-    private fun hideBar() {
-        val uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-        window.decorView.systemUiVisibility = uiFlags
     }
 
     override fun onBackPressed() {
