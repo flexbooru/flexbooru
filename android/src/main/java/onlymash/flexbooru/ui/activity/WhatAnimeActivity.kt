d4c2a1fbf871570c255258defd64098867ab092a
onlymash <im@fiepi.me>
2019-06-21 15:06:43 +0800

    Add anime player

--- android/src/main/java/onlymash/flexbooru/ui/activity/WhatAnimeActivity.kt
+++ android/src/main/java/onlymash/flexbooru/ui/activity/WhatAnimeActivity.kt
@@ -1,6 +1,7 @@
 package onlymash.flexbooru.ui.activity
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
@@ -12,17 +13,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Base64
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_what_anime.*
 import kotlinx.android.synthetic.main.common_toolbar_list.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -30,6 +32,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import onlymash.flexbooru.R
+import onlymash.flexbooru.common.Settings
+import onlymash.flexbooru.exoplayer.PlayerHolder
+import onlymash.flexbooru.extension.copyText
 import onlymash.flexbooru.extension.fileExt
 import onlymash.flexbooru.extension.toVisibility
 import onlymash.flexbooru.glide.GlideApp
@@ -38,14 +43,16 @@ import onlymash.flexbooru.tracemoe.model.TraceResponse
 import onlymash.flexbooru.tracemoe.presentation.TraceMoeActions
 import onlymash.flexbooru.tracemoe.presentation.TraceMoePresenter
 import onlymash.flexbooru.tracemoe.presentation.TraceMoeView
+import onlymash.flexbooru.ui.fragment.TransparentBottomSheetDialogFragment
 import java.io.ByteArrayOutputStream
 import kotlin.properties.Delegates
 
 private const val READ_IMAGE_REQUEST_CODE = 147
+private const val PREVIEW_VIDEO_URL_KEY = "preview_video_url"
 
 class WhatAnimeActivity : AppCompatActivity(), TraceMoeView {
 
-    private var response: TraceResponse? = null
+    private val docs: MutableList<Doc> = mutableListOf()
 
     private lateinit var whatAnimeAdapter: WhatAnimeAdapter
 
@@ -79,10 +86,14 @@ class WhatAnimeActivity : AppCompatActivity(), TraceMoeView {
                 )
             } catch (_: ActivityNotFoundException) {}
         }
+        if (!Settings.isOrderSuccess) {
+            startActivity(Intent(this, PurchaseActivity::class.java))
+            finish()
+        }
     }
 
     private fun search(uri: Uri) {
-        response = null
+        docs.clear()
         whatAnimeAdapter.notifyDataSetChanged()
         progress_bar.toVisibility(true)
         val ext = uri.toString().fileExt()
@@ -160,7 +171,16 @@ class WhatAnimeActivity : AppCompatActivity(), TraceMoeView {
     }
 
     override fun onUpdate(data: TraceResponse) {
-        response = data
+        docs.clear()
+        if (Settings.safeMode) {
+            data.docs.forEach {
+                if (!it.isAdult) {
+                    docs.add(it)
+                }
+            }
+        } else {
+            docs.addAll(data.docs)
+        }
         whatAnimeAdapter.notifyDataSetChanged()
     }
 
@@ -175,11 +195,10 @@ class WhatAnimeActivity : AppCompatActivity(), TraceMoeView {
             WhatAnimeViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_what_anime, parent, false))
 
-        override fun getItemCount(): Int = response?.docs?.size ?: 0
+        override fun getItemCount(): Int = docs.size
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val result = response?.docs?.get(position) ?: return
-            (holder as WhatAnimeViewHolder).bind(result)
+            (holder as WhatAnimeViewHolder).bind(docs[position])
         }
 
         inner class WhatAnimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -190,14 +209,29 @@ class WhatAnimeActivity : AppCompatActivity(), TraceMoeView {
             private val info2: AppCompatTextView = itemView.findViewById(R.id.info_2)
 
             fun bind(data: Doc) {
-                title.text = data.title ?: data.filename
+                val text = data.title ?: data.filename
+                title.text = text
                 info1.text = formatTime(data.at)
                 info2.text = data.anime
                 val previewUrl = "https://trace.moe/thumbnail.php?anilist_id=${data.anilistId}&file=${Uri.encode(data.filename)}&t=${data.at}&token=${data.tokenthumb}"
+                val placeholderId = if (data.isAdult) R.drawable.background_rating_e else R.drawable.background_rating_s
                 GlideApp.with(itemView.context)
                     .load(previewUrl)
+                    .placeholder(ContextCompat.getDrawable(itemView.context, placeholderId))
                     .fitCenter()
                     .into(preview)
+                itemView.setOnLongClickListener {
+                    copyText(text)
+                    true
+                }
+                itemView.setOnClickListener {
+                    AnimePlayerDialog().apply {
+                        arguments = Bundle().apply {
+                            putString(PREVIEW_VIDEO_URL_KEY, "https://media.trace.moe/video/${data.anilistId}/${Uri.encode(data.filename)}?t=${data.at}&token=${data.tokenthumb}")
+                        }
+                        show(supportFragmentManager, "player")
+                    }
+                }
             }
 
             private fun formatTime(time: Float): String {
@@ -210,6 +244,62 @@ class WhatAnimeActivity : AppCompatActivity(), TraceMoeView {
             private fun timeString(time: Int): String {
                 return if (time < 10) "0$time" else time.toString()
             }
+        }
+    }
+
+    class AnimePlayerDialog : TransparentBottomSheetDialogFragment() {
+
+        private lateinit var playerView: PlayerView
+        private lateinit var behavior: BottomSheetBehavior<View>
+
+        private var url: String? = null
+
+        private val playerHolder: PlayerHolder by lazy { PlayerHolder(requireContext()) }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            url = arguments?.getString(PREVIEW_VIDEO_URL_KEY)
+        }
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val dialog = super.onCreateDialog(savedInstanceState)
+            val view = View.inflate(context, R.layout.fragment_anime_player, null)
+            playerView = view.findViewById(R.id.exoplayer_view)
+            dialog.setContentView(view)
+            behavior = BottomSheetBehavior.from(view.parent as View)
+            behavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+                }
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                        dismiss()
+                    }
+                }
+            })
+            return dialog
+        }
+
+        override fun onResume() {
+            super.onResume()
+            url?.let {
+                playerHolder.start(Uri.parse(it), playerView)
+            }
+        }
+
+        override fun onStart() {
+            super.onStart()
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        override fun onStop() {
+            super.onStop()
+            playerHolder.stop()
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            playerHolder.release()
         }
     }
 }
