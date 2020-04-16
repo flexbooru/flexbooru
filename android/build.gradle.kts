68c9a99e2b3d707e4efec195f5a9563129629c9f
onlymash <im@fiepi.me>
2020-04-16 21:58:53 +0800

    Bump version name

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -61,7 +61,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = verCode
-        versionName = "2.3.3"
+        versionName = "2.3.4"
         versionNameSuffix = ".c$verCode"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
