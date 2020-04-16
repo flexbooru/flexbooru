401368dd5f9c4545a5bb6df273ecd3645203ac45
onlymash <im@fiepi.me>
2020-04-16 17:51:57 +0800

    Update TreeUriExt

--- android/src/main/java/onlymash/flexbooru/extension/TreeUriExt.kt
+++ android/src/main/java/onlymash/flexbooru/extension/TreeUriExt.kt
@@ -15,11 +15,9 @@
 
 package onlymash.flexbooru.extension
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.DocumentsContract
@@ -29,32 +27,20 @@ import androidx.documentfile.provider.DocumentFile
 import onlymash.flexbooru.R
 import onlymash.flexbooru.common.Settings
 import onlymash.flexbooru.common.Values.REQUEST_CODE_OPEN_DIRECTORY
+import java.io.IOException
 import java.util.*
 
 fun Activity.getSaveUri(fileName: String): Uri? = getUri("save", fileName)
 
-fun Activity.getDownloadUri(host: String, fileName: String): Uri? = getUri(host, fileName)
-
 fun Activity.getPoolUri(fileName: String): Uri? = getUri("pools", fileName)
+
+fun Activity.getDownloadUri(host: String, fileName: String): Uri? = getUri(host, fileName)
 
 fun getFileUriByDocId(docId: String): Uri? {
     val treeId = Settings.downloadDirPathTreeId ?: return null
     val authority = Settings.downloadDirPathAuthority ?: return null
     val treeUri = DocumentsContract.buildTreeDocumentUri(authority, treeId) ?: return null
     return DocumentsContract.buildDocumentUriUsingTree(treeUri, docId)
-}
-
-fun Activity.openDocumentTree() {
-    try {
-        startActivityForResult(
-            Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
-                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
-                        or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-                        or Intent.FLAG_GRANT_PREFIX_URI_PERMISSION
-                        or Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
-            },
-            REQUEST_CODE_OPEN_DIRECTORY)
-    } catch (_: ActivityNotFoundException) {}
 }
 
 private fun Activity.getUri(dirName: String, fileName: String): Uri? {
@@ -73,60 +59,64 @@ private fun Activity.getUri(dirName: String, fileName: String): Uri? {
         openDocumentTree()
         return null
     }
-    var dirDocId = findChildrenDocIdByFilename(treeUri, dirName)
-    if (dirDocId == null) {
-        treeDir.createFile(DocumentsContract.Document.MIME_TYPE_DIR, dirName)
-        dirDocId = findChildrenDocIdByFilename(treeUri, dirName) ?: return null
+    val dirId = getDocumentFileId(treeId, dirName)
+    val dirUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, dirId)
+    val dir = DocumentFile.fromSingleUri(this, dirUri)
+    try {
+        if (dir == null || !dir.exists()) {
+            treeDir.createDirectory(dirName) ?: return null
+        } else if (dir.isFile) {
+            dir.delete()
+            treeDir.createDirectory(dirName) ?: return null
+        }
+    } catch (_: IOException) {
+        return null
     }
-    val fileDocId = if (dirDocId.endsWith(":")) {
-        dirDocId + fileName
-    } else {
-        "$dirDocId/$fileName"
+    val fileId= getDocumentFileId(dirId, fileName)
+    val fileUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, fileId)
+    val file = DocumentFile.fromSingleUri(this, fileUri)
+    try {
+        if (file == null || !file.exists()) {
+            DocumentsContract.createDocument(
+                contentResolver,
+                dirUri,
+                fileName.getMimeType(),
+                fileName
+            ) ?: return null
+        } else if (file.isDirectory) {
+            file.delete()
+            DocumentsContract.createDocument(
+                contentResolver,
+                dirUri,
+                fileName.getMimeType(),
+                fileName
+            ) ?: return null
+        }
+    } catch (_: IOException) {
+        return null
     }
-    val dirDocUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, dirDocId)
-    var fileDocUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, fileDocId)
-    val fileDoc = DocumentFile.fromSingleUri(this, fileDocUri)
-    if (fileDoc == null || !fileDoc.exists()) {
-        fileDocUri = DocumentsContract.createDocument(
-            contentResolver,
-            dirDocUri,
-            fileName.getMimeType(),
-            fileName
-        )
-    }
-    return fileDocUri
+    return fileUri
 }
 
-@SuppressLint("Recycle")
-private fun Context.findChildrenDocIdByFilename(treeUri: Uri, filename: String): String? {
-    var docId: String? = null
-    val childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(
-        treeUri,
-        DocumentsContract.getTreeDocumentId(treeUri)
-    )
-    val childrenCursor = contentResolver.query(
-        childrenUri,
-        arrayOf(
-            DocumentsContract.Document.COLUMN_DISPLAY_NAME,
-            DocumentsContract.Document.COLUMN_DOCUMENT_ID
-        ),
-        null,
-        null,
-        null
-    )
+fun Activity.openDocumentTree() {
     try {
-        childrenCursor?.let {
-            while (it.moveToNext()) {
-                if ( childrenCursor.getString(0) == filename) {
-                    docId = childrenCursor.getString(1)
-                    break
-                }
-            }
-        }
-    } finally {
-        childrenCursor?.safeCloseQuietly()
+        startActivityForResult(
+            Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                        or Intent.FLAG_GRANT_PREFIX_URI_PERMISSION
+                        or Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+            },
+            REQUEST_CODE_OPEN_DIRECTORY)
+    } catch (_: ActivityNotFoundException) {}
+}
+
+private fun getDocumentFileId(prentId: String, fileName: String): String {
+    return if (prentId.endsWith(":")) {
+        prentId + fileName
+    } else {
+        "$prentId/$fileName"
     }
-    return docId
 }
 
 fun String.getMimeType(): String {
