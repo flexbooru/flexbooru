188b089559695b39fc76f34289bfd42d990187c9
onlymash <im@fiepi.me>
2021-06-10 19:24:23 +0800

    Bump version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         minSdk = 21
         targetSdk = 30
         versionCode = verCode
-        versionName = "2.7.1"
+        versionName = "2.7.2"
         versionNameSuffix = ".c$verCode"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
