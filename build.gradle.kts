be5a860c49116f612824703055b29fbf9db73964
onlymash <im@fiepi.me>
2022-01-31 00:37:12 +0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ buildscript {
         val kotlinVersion = "1.6.10"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath(kotlin("serialization", kotlinVersion))
-        classpath("com.android.tools.build:gradle:7.1.0-rc01")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
         classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
